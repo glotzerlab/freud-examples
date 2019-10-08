0a89f12a41dcde5aafdc61a958cd6153f14424d9
Bradley Dice <bdice@bradleydice.com>
2019-10-08 17:32:54 -0400

    Update Box examples.

--- util.py
+++ util.py
@@ -115,27 +115,21 @@ def make_square(nx=1, ny=1, fractions=np.array([[0, 0, 0]], dtype=np.float32),
 
 def box_2d_to_points(box):
     """Generate the points needed to plot a 2d box.
-    
+
     Args:
         box (:py:class:`freud.box.Box`): The Box to plot.
-     
+
     Returns:
         :py:class:`np.ndarray`: The set of points to plot.
-    
+
     Example::
         box = freud.box.Box.cube(L=5):
         points = box_2d_to_points(box)
         ax.plot(points[:, 0], points[:, 1])
     """
-    points = []
-    points.append(box.makeCoordinates([0, 0, 0])[:2])
-    points.append(box.makeCoordinates([0, 1, 0])[:2])
-    points.append(box.makeCoordinates([1, 1, 0])[:2])
-    points.append(box.makeCoordinates([1, 0, 0])[:2])
-    points.append(points[0])  # Need to copy this so that the box is closed.
-    points = np.array(points)
-    
-    return points
+    corners = [[0, 0, 0], [0, 1, 0], [1, 1, 0], [1, 0, 0]]
+    corners.append(corners[0])  # Need to copy this so that the box is closed.
+    return box.make_absolute(corners)[:, :2]
 
 def make_polygon(sides, radius=1):
     thetas = np.linspace(0, 2*np.pi, sides+1)[:sides]
